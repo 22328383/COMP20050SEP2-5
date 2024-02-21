f004f88e32be3905e145472b3c0d2280fc23af66
Olaf Madynski <olaf.madynski04@gmail.com>
2024-02-21 23:44:38 +0000

    Sprint 1 controller

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void mainMenu() {
+        System.out.println("Welcome To Black Box!\nPlease type START to begin or QUIT to end the program.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        switch (input) {
+            case "START":
+                drawBoard(newBoard());
+                break;
+            case "QUIT":
+                System.out.println("Ending Program...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid input detected.");
+                mainMenu();
+                break;
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        mainMenu();
+    }
+}
