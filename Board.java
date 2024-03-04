b63889d57c6e720600deabf208c169689662a003
22328383 <andrei.krivoruchko@ucdconnect.ie>
2024-03-04 18:38:46 +0000

    Circles of Influence output

--- Board.java
+++ Board.java
@@ -44,6 +44,148 @@ public class Board {
         return cellCounts;
     }
 
+    public int[] getNeighbors(int index) {
+        int[] atomPos = getCoordinates(index);
+        int x = atomPos[0];
+        int y = atomPos[1];
+
+        int[] neighborsArray = new int[6];
+
+        // Top right neighbor
+        if (y == 0) {
+            neighborsArray[0] = -1;
+        }
+        if (y == 1 || y == 8) {
+            neighborsArray[0] = index-5;
+        }
+        if (y == 2 || y == 7) {
+            neighborsArray[0] = index-6;
+        }
+        if (y == 3 || y == 6) {
+            neighborsArray[0] = index-7;
+        }
+        if (y == 4 || y == 5) {
+            neighborsArray[0] = index-8;
+        }
+        int j = 0;
+        for (int i = 0; i < y; i++) {
+            j += ROWCELLCNT[i];
+        }
+        if(neighborsArray[0] == j) {
+            neighborsArray[0] = -1;
+        }
+
+        // Right neighbor
+        if ((x + 1) < ROWCELLCNT[y]) {
+            neighborsArray[1] = index + 1;
+        } else {
+            neighborsArray[1] = -1;
+        }
+
+        // Bottom right neighbor
+        if (y == 0 || y == 7) {
+            neighborsArray[2] = index+6;
+        }
+        if (y == 1 || y == 6) {
+            neighborsArray[2] = index+7;
+        }
+        if (y == 2 || y == 5) {
+            neighborsArray[2] = index+8;
+        }
+        if (y == 3 || y == 4) {
+            neighborsArray[2] = index+9;
+        }
+        if (y == 8) {
+            neighborsArray[2] = -1;
+        } else {
+            j = 0;
+            for (int i = 0; i <= y + 1; i++) {
+                j += ROWCELLCNT[i];
+            }
+            if (neighborsArray[2] == j) {
+                neighborsArray[2] = -1;
+            }
+        }
+
+        // Bottom left neighbor
+        if (y == 0 || y == 7) {
+            neighborsArray[3] = index+5;
+        }
+        if (y == 1 || y == 6) {
+            neighborsArray[3] = index+6;
+        }
+        if (y == 2 || y == 5) {
+            neighborsArray[3] = index+7;
+        }
+        if (y == 3 || y == 4) {
+            neighborsArray[3] = index+8;
+        }
+        if (y == 8) {
+            neighborsArray[3] = -1;
+        } else {
+            j = 0;
+            for (int i = 0; i < y; i++) {
+                j += ROWCELLCNT[i];
+            }
+            j += ROWCELLCNT[y] - 1;
+            if (neighborsArray[3] == j) {
+                neighborsArray[3] = -1;
+            }
+        }
+
+        // Left neighbor
+        if ((x - 1) >= 0) {
+            neighborsArray[4] = index - 1;
+        } else {
+            neighborsArray[4] = -1;
+        }
+
+        // Top left neighbor
+        if (y == 0) {
+            neighborsArray[5] = -1;
+        }
+        if (y == 1 || y == 8) {
+            neighborsArray[5] = index-6;
+        }
+        if (y == 2 || y == 7) {
+            neighborsArray[5] = index-7;
+        }
+        if (y == 3 || y == 6) {
+            neighborsArray[5] = index-8;
+        }
+        if (y == 4 || y == 5) {
+            neighborsArray[5] = index-9;
+        }
+        if(y > 0) {
+            j = 0;
+            for (int i = 0; i < y - 2; i++) {
+                j += ROWCELLCNT[i];
+            }
+            if(neighborsArray[5] == j) {
+                neighborsArray[5] = -1;
+            }
+        }
+        if(x == 0 && y == 1) {
+            neighborsArray[5] = -1;
+        }
+
+        return neighborsArray;
+    }
+
+
+    public int[] getCoordinates(int index) {
+        int x = 0;
+        int y = allCells.get(index).getRow() - 1;
+
+        int totalCells = 0;
+        for (int i = 0; i < y; i++) {
+            totalCells += ROWCELLCNT[i];
+        }
+        x = index - totalCells;
+
+        int[] coordinates = {x, y};
+        return coordinates;
+    }
 
     public static int findRow(int cellNum, int[] cellCounts) {
         int row = 0;
--- BoardTest.java
+++ BoardTest.java
@@ -22,11 +22,11 @@ class BoardTest {
         assertEquals(1, Board.findRow(0, nineRows));
         assertEquals(1, Board.findRow(1, nineRows));
         assertEquals(2, Board.findRow(6, nineRows));
-        assertEquals(4, Board.findRow(20,nineRows));
-        assertEquals(5, Board.findRow(29,nineRows));
-        assertEquals(7, Board.findRow(46,nineRows));
-        assertEquals(9, Board.findRow(56,nineRows));
-        assertEquals(9, Board.findRow(57,nineRows));
+        assertEquals(4, Board.findRow(20, nineRows));
+        assertEquals(5, Board.findRow(29, nineRows));
+        assertEquals(7, Board.findRow(46, nineRows));
+        assertEquals(9, Board.findRow(56, nineRows));
+        assertEquals(9, Board.findRow(57, nineRows));
     }
 
     @Test
@@ -34,4 +34,81 @@ class BoardTest {
         Board newBoard = new Board();
         newBoard.drawBoard();
     }
+
+    @Test
+    void borderTest() {
+        Board newBoard = Board.newBoard();
+        // Test case for cell at index 24
+        int[] neighbors24 = newBoard.getNeighbors(24);
+        System.out.println("Neighbors of cell at index 24: " + Arrays.toString(neighbors24));
+        System.out.println("Expected output: [17, 25, 33, 32, 23, 16]");
+        System.out.println("");
+
+        // Test case for cell at index 0
+        int[] neighbors0 = newBoard.getNeighbors(0);
+        System.out.println("Neighbors of cell at index 0: " + Arrays.toString(neighbors0));
+        System.out.println("Expected output: [-1, 1, 6, 5, -1, -1]");
+        System.out.println("");
+
+        // Test case for cell at index 60
+        int[] neighbors60 = newBoard.getNeighbors(60);
+        System.out.println("Neighbors of cell at index 60: " + Arrays.toString(neighbors60));
+        System.out.println("Expected output: [55, -1, -1, -1, 59, 54]");
+        System.out.println("");
+
+        // Test case for cell at index 42
+        int[] neighbors42 = newBoard.getNeighbors(42);
+        System.out.println("Neighbors of cell at index 42: " + Arrays.toString(neighbors42));
+        System.out.println("Expected output: [34, -1, -1, 49, 41, 33]");
+        System.out.println("");
+
+        // Test case for cell at index 2
+        int[] neighbors2 = newBoard.getNeighbors(2);
+        System.out.println("Neighbors of cell at index 2: " + Arrays.toString(neighbors2));
+        System.out.println("Expected output: [-1, 3, 8, 7, 1, -1]");
+        System.out.println("");
+
+        int[] neighbors45 = newBoard.getNeighbors(45);
+        System.out.println("Neighbors of cell at index 45: " + Arrays.toString(neighbors45));
+        System.out.println("Expected output: [38, 46, 52, 51, 44, 37]");
+        System.out.println("");
+
+        int[] neighbors9 = newBoard.getNeighbors(9);
+        System.out.println("Neighbors of cell at index 9: " + Arrays.toString(neighbors9));
+        System.out.println("Expected output: [4, 10, 16, 15, 8, 3]");
+    }
+
+    @Test
+    void coordinateTest() {
+        Board newBoard = Board.newBoard();
+        // Test case for cell at index 24
+        int[] coordinates24 = newBoard.getCoordinates(24);
+        System.out.println("Coordinates of cell at index 24: x = " + coordinates24[0] + ", y = " + coordinates24[1]);
+        System.out.println("Expected output: x = 6, y = 3");
+        System.out.println("");
+
+        // Test case for cell at index 0
+        int[] coordinates0 = newBoard.getCoordinates(0);
+        System.out.println("Coordinates of cell at index 0: x = " + coordinates0[0] + ", y = " + coordinates0[1]);
+        System.out.println("Expected output: x = 0, y = 0");
+        System.out.println("");
+
+        // Test case for cell at index 60
+        int[] coordinates60 = newBoard.getCoordinates(60);
+        System.out.println("Coordinates of cell at index 60: x = " + coordinates60[0] + ", y = " + coordinates60[1]);
+        System.out.println("Expected output: x = 4, y = 8");
+        System.out.println("");
+
+        // Test case for cell at index 42
+        int[] coordinates42 = newBoard.getCoordinates(42);
+        System.out.println("Coordinates of cell at index 42: x = " + coordinates42[0] + ", y = " + coordinates42[1]);
+        System.out.println("Expected output: x = 7, y = 5");
+        System.out.println("");
+
+        // Test case for cell at index 2
+        int[] coordinates2 = newBoard.getCoordinates(2);
+        System.out.println("Coordinates of cell at index 2: x = " + coordinates2[0] + ", y = " + coordinates2[1]);
+        System.out.println("Expected output: x = 2, y = 0");
+    }
+
 }
